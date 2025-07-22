989743bf3aadc700acc01021671c6c70f0cb981f
rizky-ekaputra <87692613+rizky-ekaputra@users.noreply.github.com>
2025-07-22 17:56:51 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,64 +1,26 @@
-const { createApp, ref, onMounted } = Vue;
+const { createApp } = Vue;
 
 createApp({
-  setup() {
-    const count = ref(0);
-    const bounce = ref(false);
-    const mouseX = ref(50);
-    const mouseY = ref(50);
-    const audio = new Audio("https://www.bensound.com/bensound-music/bensound-dubstep.mp3");
-    const isPlaying = ref(false);
-
-    const updateMouse = (e) => {
-      mouseX.value = (e.clientX / window.innerWidth) * 100;
-      mouseY.value = (e.clientY / window.innerHeight) * 100;
-    };
-
-    const increment = () => {
-      count.value++;
-      bounce.value = true;
-    };
-
-    const toggleMusic = () => {
-      if (isPlaying.value) {
-        audio.pause();
-      } else {
-        audio.play();
-      }
-      isPlaying.value = !isPlaying.value;
-    };
-
-    onMounted(() => {
-      window.addEventListener("mousemove", updateMouse);
-    });
-
+  data() {
     return {
-      count,
-      bounce,
-      increment,
-      mouseX,
-      mouseY,
-      toggleMusic,
-      isPlaying,
+      count: 0,
+      bounce: false,
+      mouseX: 0,
+      mouseY: 0,
     };
   },
-  template: `
-    <div class="app" @mousemove="updateMouse">
-      <div class="aurora-background"
-           :style="{ backgroundPosition: mouseX + '%' + ' ' + mouseY + '%' }"></div>
-      <div class="starfield"></div>
-
-      <h1 class="title">Aurora Pulse Counter</h1>
-      <p class="developed-by">Developed by: Rizky</p>
-
-      <div :class="['counter-circle', bounce ? 'bounce' : '']" @animationend="bounce = false">
-        {{ count }}
-      </div>
-
-      <button class="count-button" @click="increment">Count</button>
-      <button class="music-button" @click="toggleMusic">
-        {{ isPlaying ? 'Pause Music' : 'Play Music' }}
-      </button>
-    </div>
-  `
-}).mount("#app");
+  methods: {
+    increment() {
+      this.count++;
+      this.bounce = true;
+    },
+    updateMouse(e) {
+      const centerX = window.innerWidth / 2;
+      const centerY = window.innerHeight / 2;
+      const offsetX = (e.clientX - centerX) * 0.02;
+      const offsetY = (e.clientY - centerY) * 0.02;
+      this.mouseX = offsetX;
+      this.mouseY = offsetY;
+    },
+  },
+}).mount('#app');
